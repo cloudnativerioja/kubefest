2b936ce9c391e62fb91e7dd0f9cbcb508a875f89
Adrian Barrio <statickidz@gmail.com>
2025-01-30 12:57:30 +0100

    refactor: swap Cloudflare API token and zone ID variable definitions

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,11 @@
-variable "cloudflare_zone_id" {
-  type        = string
-  default     = ""
-  description = "Cloudflare Zone ID"
-}
-
 variable "cloudflare_api_token" {
   type        = string
   default     = ""
   description = "Cloudflare API Token"
+}
+
+variable "cloudflare_zone_id" {
+  type        = string
+  default     = ""
+  description = "Cloudflare Zone ID"
 }
