0273599b15d2604e14e68a7e21e3e6ea2f6b2ef8
Adrian Barrio <statickidz@gmail.com>
2025-01-30 11:56:05 +0100

    refactor: update infrastructure workflows and backend configuration

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "cloudnative-cloudflare"
-    key    = "tf-state/kubefest.tfstate"
-    region = "us-east-1"
-  }
-
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
